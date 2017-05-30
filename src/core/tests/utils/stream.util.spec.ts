9e3c9266edabd71b2038548786a6ac21a09ea1aa
thomrick <tricart@xebia.fr>
2017-05-30 10:38:55 +0200

    remove stream utils to change design

--- src/core/tests/utils/stream.util.spec.ts
+++
@@ -1,9 +0,0 @@
-import {StreamUtil} from '../../utils/stream.util';
-
-describe('StreamUtil', () => {
-  describe('#replace()', () => {
-    it('can call replace()', () => {
-      StreamUtil.replace('[NAME]', 'ClassName');
-    });
-  });
-});
--- src/core/utils/stream.util.ts
+++
@@ -1,7 +0,0 @@
-import {Stream, Transform} from 'stream';
-
-export class StreamUtil {
-  static replace(origin: string, destination: string): Transform {
-    return null;
-  }
-}
