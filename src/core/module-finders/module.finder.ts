8d45d4905904a34aac037aeeca640cfa4ec8bf8b
thomrick <tricart@xebia.fr>
2017-06-07 19:26:24 +0200

    develop the module file finder

--- src/core/module-finders/module.finder.ts
+++ src/core/module-finders/module.finder.ts
@@ -1,8 +1,28 @@
 import {ModuleFinder} from '../../common/interfaces/module.finder.interface';
+import {FileSystemUtils} from '../utils/file-system.utils';
+import * as path from 'path';
+import {isNullOrUndefined} from 'util';
 
 export class ModuleFinderImpl implements ModuleFinder {
+  private MODULE_FILENAME_REGEX = /.*.module.(ts|js)/;
+
   public findFrom(origin: string): Promise<string> {
-    throw new Error("Method not implemented.");
+    const dirname = path.dirname(origin);
+    return this.findIn(dirname);
+  }
+
+  private findIn(dirname: string): Promise<string> {
+    return FileSystemUtils.readdir(dirname)
+      .then(files => {
+        const filename: string = files.find(filename => this.MODULE_FILENAME_REGEX.test(filename));
+        if (isNullOrUndefined(filename)) {
+          const elements = dirname.split(path.sep);
+          const parent = elements.splice(0, elements.length - 1).join(path.sep);
+          return this.findIn(parent);
+        } else {
+          return path.join(dirname, files.find(filename => this.MODULE_FILENAME_REGEX.test(filename)));
+        }
+      });
   }
 
 }
