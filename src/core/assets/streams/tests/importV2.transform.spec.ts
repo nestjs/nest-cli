ccb4d55792936e372edf5161dd90697982becff6
thomrick <tricart@xebia.fr>
2017-06-24 19:01:27 +0200

    add commit missing files

---
+++ src/core/assets/streams/tests/importV2.transform.spec.ts
@@ -0,0 +1,38 @@
+import {Readable, Transform, Writable} from 'stream';
+import {BufferedReadable, BufferedWritable} from './test.utils';
+import {expect} from 'chai';
+import {ImportTransformV2} from '../importV2.transform';
+
+describe('ImportTransformV2', () => {
+  const content: string = '' +
+    'import {Module} from \'@nestjs/common\';\n' +
+    '\n' +
+    '@Module({})\n' +
+    'export class AssetModule {}\n';
+  const className: string = 'AssetService';
+  const dirname: string = './path/to/asset/asset.service';
+
+  let reader: Readable;
+  let writer: Writable;
+  beforeEach(() => {
+    reader = new BufferedReadable(Buffer.from(content));
+    writer = new BufferedWritable();
+  });
+
+  let transform: Transform;
+  beforeEach(() => transform = new ImportTransformV2());
+
+  it('should add the imported asset', done => {
+    const newContent: string =
+      'import {Module} from \'@nestjs/common\';\n' +
+      `import {__CLASS_NAME__} from __DIR_NAME__;\n` +
+      '\n' +
+      '@Module({})\n' +
+      'export class AssetModule {}\n';
+    reader.on('end', () => {
+      expect((writer as BufferedWritable).getBuffer().toString()).to.be.equal(newContent);
+      done();
+    });
+    reader.pipe(transform).pipe(writer);
+  });
+});
---
+++ src/core/assets/streams/tests/metadataV2.transform.spec.ts
@@ -0,0 +1,77 @@
+import {Readable, Transform, Writable} from 'stream';
+import {AssetEnum} from '../../../../common/asset/enums/asset.enum';
+import {BufferedReadable, BufferedWritable} from './test.utils';
+import {expect} from 'chai';
+import {MetadataTransformV2} from '../metadataV2.transform';
+
+describe('MetadataTransformV2', () => {
+  let reader: Readable;
+  let writer: Writable;
+
+  let transform: Transform;
+  beforeEach(() => transform = new MetadataTransformV2(AssetEnum.COMPONENT));
+
+  context('no module metadata for asset type', () => {
+    const content: string =
+      'import {Module} from \'@nestjs/common\';\n' +
+      '\n' +
+      '@Module({})\n' +
+      'export class AssetModule {}\n';
+
+    beforeEach(() => {
+      reader = new BufferedReadable(Buffer.from(content));
+      writer = new BufferedWritable();
+    });
+    it('should update the module metadata', done => {
+      const newContent: string =
+        'import {Module} from \'@nestjs/common\';\n' +
+        '\n' +
+        '@Module({\n' +
+        '  components: [\n' +
+        '    __CLASS_NAME__\n' +
+        '  ]\n' +
+        '})\n' +
+        'export class AssetModule {}\n';
+      reader.on('end', () => {
+        expect((writer as BufferedWritable).getBuffer().toString()).to.be.equal(newContent);
+        done();
+      });
+      reader.pipe(transform).pipe(writer);
+    });
+  });
+
+  context('existing metadata for asset type', () => {
+    const content: string =
+      'import {Module} from \'@nestjs/common\';\n' +
+      '\n' +
+      '@Module({\n' +
+      '  components: [\n' +
+      '    OtherAssetService\n' +
+      '  ]\n' +
+      '})\n' +
+      'export class AssetModule {}\n';
+
+    beforeEach(() => {
+      reader = new BufferedReadable(Buffer.from(content));
+      writer = new BufferedWritable();
+    });
+    it('should update the module metadata', done => {
+      const newContent: string =
+        'import {Module} from \'@nestjs/common\';\n' +
+        '\n' +
+        '@Module({\n' +
+        '  components: [\n' +
+        '    OtherAssetService,\n' +
+        '    __CLASS_NAME__\n' +
+        '  ]\n' +
+        '})\n' +
+        'export class AssetModule {}\n';
+      reader.on('end', () => {
+        expect((writer as BufferedWritable).getBuffer().toString()).to.be.equal(newContent);
+        done();
+      });
+      reader.pipe(transform).pipe(writer);
+    });
+  });
+});
+
