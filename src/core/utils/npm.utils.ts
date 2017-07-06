98359a31958ba699c1c93c6781511476ffb9cfac
thomrick <tricart@xebia.fr>
2017-07-06 21:23:08 +0200

    update typescript and ts-node dev dependencies

--- src/core/utils/npm.utils.ts
+++ src/core/utils/npm.utils.ts
@@ -2,32 +2,22 @@ import {ChildProcess, spawn} from 'child_process';
 
 export class NpmUtils {
   public static update(dependencies: string[] = []): Promise<void> {
-    return new Promise<void>((resolve, reject) => {
-      const spawned: ChildProcess = spawn('npm', [ 'update', '--save', ...dependencies ]);
-      spawned.on('exit', code => {
-        if (code === 1)
-          reject();
-        else
-          resolve();
-      });
-    });
+    const spawned: ChildProcess = spawn('npm', [ 'update', '--save', ...dependencies ]);
+    return this.handle(spawned);
   }
 
   public static uninstall(dependencies: string[] = []): Promise<void> {
-    return new Promise<void>((resolve, reject) => {
-      const spawned: ChildProcess = spawn('npm', [ 'uninstall', '--save', ...dependencies ]);
-      spawned.on('exit', code => {
-        if (code === 1)
-          reject();
-        else
-          resolve();
-      });
-    });
+    const spawned: ChildProcess = spawn('npm', [ 'uninstall', '--save', ...dependencies ]);
+    return this.handle(spawned);
   }
 
-  public static install(dependencies: string[]): Promise<void> {
+  public static install(dependencies: string[] = []): Promise<void> {
+    const spawned: ChildProcess = spawn('npm', [ 'install', '--save', ...dependencies ]);
+    return this.handle(spawned);
+  }
+
+  private static handle(spawned: ChildProcess): Promise<void> {
     return new Promise<void>((resolve, reject) => {
-      const spawned: ChildProcess = spawn('npm', [ 'install', '--save', ...dependencies ]);
       spawned.on('exit', code => {
         if (code === 1)
           reject();
