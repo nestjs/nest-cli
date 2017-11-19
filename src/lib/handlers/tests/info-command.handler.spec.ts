62850f57fc0a917bee0bda7b42ca1df3c387c10e
thomrick <tricart@xebia.fr>
2017-11-19 11:50:01 +0100

    fix unit tests + create Vagrant to test nest-cli in a nice environment

--- src/lib/handlers/tests/info-command.handler.spec.ts
+++ src/lib/handlers/tests/info-command.handler.spec.ts
@@ -1,33 +1,35 @@
 import * as sinon from 'sinon';
-import { CommandHandler } from '../../../common/program/interfaces/command.handler.interface';
-import { ConfigurationService } from '../../../core/configuration/configuration.service';
-import { InfoCommandHandler } from '../info-command.handler';
+import * as fs from 'fs';
+import {CommandHandler} from '../../../common/program/interfaces/command.handler.interface';
+import {InfoCommandHandler} from '../info-command.handler';
+import * as path from 'path';
 
 
 describe('InfoCommandHandler', () => {
-    let sandbox: sinon.SinonSandbox;
-    beforeEach(() => sandbox = sinon.sandbox.create());
-    afterEach(() => sandbox.restore());
+  let sandbox: sinon.SinonSandbox;
+  beforeEach(() => sandbox = sinon.sandbox.create());
+  afterEach(() => sandbox.restore());
 
-    let handler: CommandHandler;
-    beforeEach(() => {
-        handler = new InfoCommandHandler();
+  let handler: CommandHandler;
+  beforeEach(() => handler = new InfoCommandHandler());
+
+  let readFileStub: sinon.SinonStub;
+  beforeEach(() => {
+    readFileStub = sandbox.stub(fs, 'readFile').callsFake((filename, encoding, callback) => {
+      if (filename === path.join(process.cwd(), 'node_modules/@nestjs/core/package.json')) {
+        callback(undefined, JSON.stringify({ version: 'version' }));
+      } else if (filename === path.resolve('./package.json')) {
+        callback(undefined, JSON.stringify({ version: 'version' }));
+      } else {
+        callback(new Error('Should not be there'));
+      }
     });
+  });
 
-    let loadStub: sinon.SinonStub;
-    let getPropertyStub: sinon.SinonStub;
-    beforeEach(() => {
-        loadStub = sandbox.stub(ConfigurationService, 'load').callsFake(() => Promise.resolve());
-        getPropertyStub = sandbox.stub(ConfigurationService, 'getProperty').callsFake(() => 'ts')
+  describe('#execute()', () => {
+    it('should run the command handler', async () => {
+      await handler.execute({}, {}, console);
+      sinon.assert.called(readFileStub);
     });
-
-    describe('#execute()', () => {
-        it('should run the command handler', () => {
-            return handler.execute({}, {}, console)
-                .then(() => {
-                    sinon.assert.calledOnce(loadStub);
-                });
-        });
-
-    });
+  });
 });
