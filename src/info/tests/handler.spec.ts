b710e1fb744030db3fac210a2edbf7f1d0f51bb8
thomrick <tricart@xebia.fr>
2017-11-25 00:26:44 +0100

    #refactoring : refactor info command

---
+++ src/info/tests/handler.spec.ts
@@ -0,0 +1,33 @@
+import * as sinon from 'sinon';
+import { InfoHandler } from '../handler';
+import * as fs from 'fs';
+import * as path from 'path';
+
+describe('InfoHandler', () => {
+  let sandbox: sinon.SinonSandbox;
+  beforeEach(() => sandbox = sinon.sandbox.create());
+  afterEach(() => sandbox.restore());
+
+  let handler: InfoHandler;
+  beforeEach(() => handler = new InfoHandler());
+
+  let readFileStub: sinon.SinonStub;
+  beforeEach(() => {
+    readFileStub = sandbox.stub(fs, 'readFile').callsFake((filename, encoding, callback) => {
+      if (filename === path.join(process.cwd(), 'node_modules/@nestjs/core/package.json')) {
+        callback(undefined, JSON.stringify({ version: 'version' }));
+      } else if (filename === path.resolve('./package.json')) {
+        callback(undefined, JSON.stringify({ version: 'version' }));
+      } else {
+        callback(new Error('Should not be there'));
+      }
+    });
+  });
+
+  describe('#handle()', () => {
+    it('should run the command handler', async () => {
+      await handler.handle();
+      sinon.assert.called(readFileStub);
+    });
+  });
+});
