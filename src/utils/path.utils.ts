0ab13ae5534924a3eae2ae026685db8e5cbaed99
thomrick <tricart@xebia.fr>
2017-12-20 08:51:54 +0100

    #refactoring : add guard and exception assets

--- src/utils/path.utils.ts
+++
@@ -1,8 +0,0 @@
-import * as path from 'path';
-export class PathUtils {
-
-  public static relative(origin: string, destination: string): string {
-    const originDirName: string = path.dirname(origin);
-    return destination.replace(originDirName, '.');
-  }
-}
