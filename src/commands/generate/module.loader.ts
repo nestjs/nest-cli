33def520f5007c88ebd94d600de1d345a965a276
thomrick <tricart@xebia.fr>
2017-12-21 14:08:16 +0100

    #refactoring : fix module loader bug when generate a module asset

--- src/commands/generate/module.loader.ts
+++ src/commands/generate/module.loader.ts
@@ -6,33 +6,54 @@ export class ModuleLoader {
   constructor() {}
 
   public async load(asset: Asset): Promise<Asset> {
-    const files: string[] = await this.readAssetDirectory(asset.directory);
+    const directory: string = await this.findModuleDirectoryFrom(asset.directory, asset.filename);
+    const files: string[] = await this.read(directory);
     const filename: string = files.find((filename) => filename.indexOf('module') !== -1);
     const content: string = await this.getModuleContent(path.join(asset.directory, filename));
     return {
       type: 'module',
-      name: asset.name,
+      name: '',
       template: {
         name: '',
         content: content
       },
-      directory: asset.directory,
+      directory: directory,
       filename: filename
     };
+  }
+
+  private async findModuleDirectoryFrom(directory: string, assetFilename: string): Promise<string> {
+    const files: string[] = await this.read(directory);
+    if (files.find((filename) => this.isAModule(filename) && filename !== assetFilename) !== undefined) {
+      return directory;
+    } else {
+      const parent = this.computeParentPathFrom(directory);
+      return this.findModuleDirectoryFrom(parent, assetFilename);
+    }
+  }
+
+  private async read(directory: string): Promise<string[]> {
+    return new Promise<string[]>((resolve) => {
+      fs.readdir(directory, (error: NodeJS.ErrnoException, files: string[]) => {
+        resolve(files);
+      });
+    });
+  }
+
+  private isAModule(filename) {
+    return filename.indexOf('module') !== -1;
+  }
+
+  private computeParentPathFrom(directory: string): string {
+    const elements: string[] = directory.split(path.sep);
+    elements.pop();
+    return elements.join(path.sep);
   }
 
   private async getModuleContent(moduleFilename: string): Promise<string> {
     return new Promise<string>((resolve) => {
       fs.readFile(moduleFilename, (error: NodeJS.ErrnoException, buffer: Buffer) => {
         resolve(buffer.toString());
-      });
-    });
-  }
-
-  private async readAssetDirectory(dirname: string): Promise<string[]> {
-    return new Promise<string[]>((resolve) => {
-      fs.readdir(dirname, (error: NodeJS.ErrnoException, files: string[]) => {
-        resolve(files);
       });
     });
   }
