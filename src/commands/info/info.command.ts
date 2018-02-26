fa06e0617588357a4879232339e7b77d809101c0
thomrick <tricart@xebia.fr>
2018-02-26 20:04:37 +0100

    #schematics : refactor info command

--- src/commands/info/info.command.ts
+++ src/commands/info/info.command.ts
@@ -2,14 +2,14 @@ import { InfoHandler } from './info.handler';
 import { LoggerService } from '../../logger/logger.service';
 
 export class InfoCommand {
-  constructor() {}
+  constructor(private handler: InfoHandler) {}
 
   public declare(program) {
     program
       .command('info', 'Display Nest CLI information.')
       .action(async (args, options, logger) => {
         LoggerService.setLogger(logger);
-        await new InfoHandler().handle();
+        await this.handler.handle();
       });
   }
 }
