3b231ce7c1d86259b4b4f4a927e4cbbcf20159f0
thomrick <tricart@xebia.fr>
2018-03-28 20:37:35 +0200

    #reniew : develop the generate command

--- actions/generate.js
+++ actions/generate.js
@@ -1,3 +1,23 @@
-module.exports = (args, options, logger) => {
+const path = require('path');
+const SchematicRunner = require('../schematics/runner');
+const Schematic = require('../schematics/schematic');
 
+module.exports = (args, options, logger) => {
+  const runner = new SchematicRunner(logger);
+  const builder = Schematic
+    .Builder()
+    .withCollectionName(path.join(__dirname, '..', 'node_modules/.bin/schematics'))
+    .withSchematicName(args.schematic)
+    .withArgs(parse(args))
+    .withOptions(options);
+  runner.run(builder.build());
 };
+
+function parse(args) {
+  return Object.keys(args).reduce((parsed, key) => {
+    if (key !== 'schematic') {
+      parsed[key] = args[key];
+    }
+    return parsed;
+  }, {})
+}
