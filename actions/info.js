d741fe5b3a03f18cf44a4810a1b68e427c752b07
thomrick <tricart@xebia.fr>
2018-03-29 23:05:05 +0200

    #nest-v4 : review the info command to display more information

--- actions/info.js
+++ actions/info.js
@@ -1,3 +1,108 @@
-module.exports = (args, options, logger) => {
+const fs = require('fs');
+const path = require('path');
+const chalk = require('chalk');
+const os = require('os');
+const osName = require('os-name');
+const exec = require('child_process').exec;
 
+module.exports = (args, options, logger) => {
+  return displayNestjs(logger)
+    .then(() => displaySystemInformation(logger))
+    .then(() => displayNestInformation(logger));
 };
+
+function displayNestjs(logger) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path.join(__dirname, '../nest-ascii.txt'), (error, buffer) => {
+      if (error !== undefined && error !== null) {
+        reject(error);
+      } else {
+        logger.info(chalk.red(buffer.toString()));
+        resolve();
+      }
+    });
+  });
+}
+
+function displaySystemInformation(logger) {
+  logger.info(chalk.green('[System Information]'));
+  logger.info('OS Version     :', chalk.blue(osName(os.platform(), os.release())));
+  logger.info('NodeJS Version :', chalk.blue(process.version));
+  return getNpmVersion()
+    .then((npmVersion) => {
+      logger.info('NPM Version    :', chalk.blue(npmVersion));
+    })
+    .catch(() => {
+      logger.error('NPM Version    :', chalk.red('Unknown'));
+    });
+}
+
+function getNpmVersion() {
+  return new Promise((resolve, reject) => {
+    const child = exec('npm -v');
+    child.stdout.on('data', (data) => resolve(data.toString()));
+    child.stderr.on('error', (error) => reject(error));
+  });
+}
+
+function displayNestInformation(logger) {
+  logger.info(chalk.green('[Nest Information]'));
+  return readProjectPackageJsonDependencies()
+    .then((dependencies) => displayNestVersions(logger, dependencies))
+    .catch(() => logger.error('Can not read your project package.json file, are you on your project folder ?'));
+}
+
+function readProjectPackageJsonDependencies() {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path.join(process.cwd(), 'package.json'), (error, buffer) => {
+      if (error !== undefined && error !== null) {
+        reject(error);
+      } else {
+        resolve(JSON.parse(buffer).dependencies);
+      }
+    });
+  });
+}
+
+function displayNestVersions(logger, dependencies) {
+  buildNestVersionsMessage(dependencies)
+    .forEach((dependency) => logger.info(dependency.name, chalk.blue(dependency.value)));
+}
+
+function buildNestVersionsMessage(dependencies) {
+  const nestDependencies = collectNestDependencies(dependencies);
+  return format(nestDependencies);
+}
+
+function collectNestDependencies(dependencies) {
+  const nestDependencies = [];
+  Object.keys(dependencies).forEach((key) => {
+    if (key.indexOf('@nestjs') > -1) {
+      nestDependencies.push({
+        name: `${ key.replace(/@nestjs\//, '') } version`,
+        value: dependencies[ key ]
+      });
+    }
+  });
+  return nestDependencies;
+}
+
+function format(dependencies) {
+  const sorted = dependencies.sort((dependencyA, dependencyB) => dependencyA.name.length - dependencyB.name.length < 0);
+  const length = sorted[0].name.length;
+  sorted.forEach((dependency) => {
+    if (dependency.name.length < length) {
+      dependency.name = rightPad(dependency.name, length);
+    }
+    dependency.name = dependency.name.concat(' :');
+    dependency.value = dependency.value.replace(/(\^|\~)/, '');
+  });
+  return sorted;
+}
+
+function rightPad(name, length) {
+  while (name.length < length) {
+    name = name.concat(' ');
+  }
+  return name;
+}
