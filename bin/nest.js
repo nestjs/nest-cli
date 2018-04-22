adfdfc16a676c80f8855d45e4f572cdc2ea928e2
thomrick <tricart@xebia.fr>
2018-04-22 15:19:44 +0200

    refactor(schematics) : refactor design implementation

--- bin/nest.js
+++ bin/nest.js
@@ -6,13 +6,4 @@ program
 require('../commands/new')(program);
 require('../commands/generate')(program);
 require('../commands/info')(program);
-
-const { PackageManager, PackageManagerFactory } = require('../lib/package-managers');
-program
-  .command('test')
-  .action((args, options, logger) => {
-    const manager = PackageManagerFactory.create(PackageManager.YARN, logger);
-    logger.info(manager);
-  });
-
 program.parse(process.argv);
