b360306cba2c19a29b92ffaf8599579b4c68292f
thomrick <tricart@xebia.fr>
2018-04-26 21:05:41 +0200

    dept(typescript) : migrate generate command to typescript

--- actions/generate.action.ts
+++ actions/generate.action.ts
@@ -1,7 +1,37 @@
 import { AbstractAction } from './abstract.action';
+import { CollectionFactory, Collection, SchematicOption } from '../lib/schematics';
+
+interface Inputs {
+  schematic: string;
+  name: string;
+  path?: string;
+}
+
+interface Options {
+  dryRun?: boolean
+}
 
 export class GenerateAction extends AbstractAction {
-  public async handle(args: any, options: any, logger: any) {
-    throw new Error("Method not implemented.");
+  public async handle(args: Inputs, options: Options, logger: any) {
+    await generate(args, options, logger);
   }
+}
+
+const generate = async (args: Inputs, options: Options, logger: any) => {
+  const collection = CollectionFactory.create(Collection.NESTJS, logger);
+  const schematicOptions = parse(args, options);
+  return collection.execute(args.schematic, schematicOptions);
+}
+
+const parse = (args: Inputs, options: Options) => {
+  const schematicOptions: SchematicOption[] = [];
+  Object.keys(args).forEach((key) => {
+    if (key !== 'schematic') {
+      schematicOptions.push(new SchematicOption(key, args[ key ]));
+    }
+  });
+  Object.keys(options).forEach((key) => {
+    schematicOptions.push(new SchematicOption(key, options[ key ] !== undefined));
+  });
+  return schematicOptions;
 }
--- commands/command.loader.ts
+++ commands/command.loader.ts
@@ -1,12 +1,12 @@
-import { NewCommand } from "./new.command";
-import { NewAction, GenerateAction, InfoAction } from "../actions";
-import { GenerateCommand } from "./generate.command";
-import { InfoCommand } from "./info.command";
+import { NewCommand } from './new.command';
+import { NewAction, GenerateAction, InfoAction } from '../actions';
+import { GenerateCommand } from './generate.command';
+import { InfoCommand } from './info.command';
 
 export class CommandLoader {
   public static load(program): void {
     new NewCommand(new NewAction()).load(program);
-    // new GenerateCommand(new GenerateAction()).load(program);
+    new GenerateCommand(new GenerateAction()).load(program);
     // new InfoCommand(new InfoAction()).load(program);
   }
 }
