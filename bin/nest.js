5c2d84edaf35835113575126290c20cd11c80eff
thomrick <tricart@xebia.fr>
2018-04-26 21:45:25 +0200

    dept(typescript) : migrate bin to typescript

--- bin/nest.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-const program = require('caporal');
-const { CommandLoader } = require('../commands');
-program
-  .version(require('../package.json').version)
-  .description(require('../package.json').description);
-CommandLoader.load(program);
-program.parse(process.argv);
