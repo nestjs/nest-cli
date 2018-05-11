5e8113651e04c259359b76f984b9788d730cd7c9
thomrick <tricart@xebia.fr>
2018-05-11 11:22:05 +0200

    fix(new) : manage parenthesis in schematic options

--- lib/schematics/schematic.option.ts
+++ lib/schematics/schematic.option.ts
@@ -1,13 +1,28 @@
 import { strings }from '@angular-devkit/core';
 
 export class SchematicOption {
+  private OPENED_PARENTHESIS = '(';
+  private CLOSED_PARENTHESIS = ')';
   constructor(private name: string, private value: boolean | string) {}
 
   public toCommandString(): string {
     if (typeof this.value === 'string') {
-      return `--${ strings.dasherize(this.name) }=${ strings.dasherize(this.value) }`;
+      return `--${ strings.dasherize(this.name) }=${ this.format() }`;
     } else {
       return `--${ strings.dasherize(this.name) }=${ this.value }`;
     }
+  }
+
+  private format(): string {
+    return strings
+      .dasherize(this.value as string)
+      .split('')
+      .reduce((content, char) => {
+        if (char === '(' || char === ')') {
+          return `${ content }\\${ char }`;
+        } else {
+          return `${ content }${ char }`;
+        }
+      }, '');
   }
 }
