76c701fcdebd61661511fec07548752d3fa8953e
thomrick <tricart@xebia.fr>
2018-05-12 11:18:30 +0200

    dept(cli-framework) : remove logger from schematics and use console instead

--- lib/schematics/collection.factory.ts
+++ lib/schematics/collection.factory.ts
@@ -3,15 +3,14 @@ import { Collection } from './collection';
 import { NestCollection } from './nest.collection';
 import { CustomCollection } from './custom.collection';
 import { AbstractCollection } from './abstract.collection';
-import { SchematicLogger } from './schematic.logger';
 
 export class CollectionFactory {
-  public static create(collection: Collection, logger: SchematicLogger): AbstractCollection {
+  public static create(collection: Collection): AbstractCollection {
     switch (collection) {
       case Collection.NESTJS:
-        return new NestCollection(RunnerFactory.create(Runner.SCHEMATIC, logger));
+        return new NestCollection(RunnerFactory.create(Runner.SCHEMATIC));
       default:
-        return new CustomCollection(collection, RunnerFactory.create(Runner.SCHEMATIC, logger)); 
+        return new CustomCollection(collection, RunnerFactory.create(Runner.SCHEMATIC)); 
     }
   }
 }
