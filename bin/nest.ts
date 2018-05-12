167c42ca37338112da624217198080141acb21a5
thomrick <tricart@xebia.fr>
2018-05-12 10:01:08 +0200

    dept(cli-framework) : uninstall caporal to use commander only)

--- bin/nest.ts
+++ bin/nest.ts
@@ -1,13 +1,16 @@
 #!/usr/bin/env node
-import * as program from 'caporal';
+import * as commander from 'commander';
 import { CommandLoader } from '../commands';
 
 const bootstrap = () => {
-  program
+  // program
+  //   .version(require('../package.json').version)
+  //   .description(require('../package.json').description);
+  // CommandLoader.load(program);
+  // program.parse(process.argv);
+  commander
     .version(require('../package.json').version)
-    .description(require('../package.json').description);
-  CommandLoader.load(program);
-  program.parse(process.argv);
+  commander.parse(process.argv);
 }
 
 bootstrap();
