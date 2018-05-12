f549bbcb8e0269b3d42a2fef730fe01bcc661189
thomrick <tricart@xebia.fr>
2018-05-12 11:16:31 +0200

    dept(cli-framework) : remove logger from package-managers and use console instead

--- lib/package-managers/yarn.package-manager.ts
+++ lib/package-managers/yarn.package-manager.ts
@@ -1,14 +1,11 @@
-import * as ora from 'ora';
-import chalk from 'chalk';
 import { AbstractPackageManager } from './abstract.package-manager';
 import { RunnerFactory, Runner } from '../runners';
 import { PackageManager } from './package-manager';
 import { messages } from '../ui';
-import { PackageManagerLogger } from './package-manager.logger';
 
 export class YarnPackageManager extends AbstractPackageManager {
-  constructor(logger: PackageManagerLogger) {
-    super(RunnerFactory.create(Runner.YARN, logger), logger);
+  constructor() {
+    super(RunnerFactory.create(Runner.YARN));
   }
 
   public get name() {
