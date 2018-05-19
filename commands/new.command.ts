ef39bfd6e15af552430d7d45d9cbb4cec7ceeb37
thomrick <tricart@xebia.fr>
2018-05-19 11:29:30 +0200

    feature(skip-install) : add skip-install option in new command

--- commands/new.command.ts
+++ commands/new.command.ts
@@ -10,9 +10,11 @@ export class NewCommand extends AbstractCommand {
       .alias('n')
       .description('Generate a new Nest application.')
       .option('--dry-run', 'Allow to test changes before execute command.')
+      .option('--skip-install', 'Allow to skip package installation.')
       .action(async (name: string, description: string, version: string, author: string, command: Command) => {
         const options: Input[] = [];
         options.push(parse('dry-run')(command[ 'dryRun' ] !== undefined ? command[ 'dryRun' ] : false));
+        options.push(parse('skip-install')(command[ 'skip-install' ] !== undefined ? command[ 'skip-install' ] : false));
         const inputs: Input[] = [];
         inputs.push(parse('name')(name));
         inputs.push(parse('description')(description));
