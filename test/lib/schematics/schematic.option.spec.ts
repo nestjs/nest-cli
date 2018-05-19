28f6603b4f42806f741452b164191058de1926eb
thomrick <tricart@xebia.fr>
2018-05-19 17:35:02 +0200

    fix() : better schematic option management on new and generate commands

--- test/lib/schematics/schematic.option.spec.ts
+++ test/lib/schematics/schematic.option.spec.ts
@@ -1,50 +1,71 @@
 import { SchematicOption } from '../../../lib/schematics';
 
 describe('Schematic Option', () => {
-  // it('should manage boolen option', () => {
-  //   const option = new SchematicOption('dryRun', false);
-  //   expect(option.toCommandString()).toEqual('--dry-run=false');
-  // });
   [
     {
-      description: 'should manage string option',
+      description: 'should manage boolean option',
+      option: 'dry-run',
       input: false,
       expected: false
     },
     {
-      description: 'should manage string option',
+      description: 'should manage string option name',
+      option: 'name',
       input: 'my-app',
       expected: 'my-app'
     },
     {
-      description: 'should manage spaced string option value',
+      description: 'should manage spaced string option value name',
+      option: 'name',
       input: 'my app',
       expected: 'my-app'
     },
     {
-      description: 'should manage camelcased string option value',
+      description: 'should manage camelcased string option value name',
+      option: 'name',
       input: 'myApp',
       expected: 'my-app'
     },
     {
-      description: 'should manage classified string option value',
+      description: 'should manage classified string option value name',
+      option: 'name',
       input: 'MyApp',
       expected: 'my-app'
     },
     {
-      description: 'should manage parenthesis string option value',
+      description: 'should manage parenthesis string option value name',
+      option: 'name',
       input: 'my-(app)',
       expected: 'my-\\(app\\)'
     },
     {
-      description: 'should manage brackets string option value',
+      description: 'should manage brackets string option value name',
+      option: 'name',
       input: 'my-[app]',
       expected: 'my-\\[app\\]'
+    },
+    {
+      description: 'should manage description',
+      option: 'description',
+      input: 'My super app',
+      expected: '"My super app"'
+    },
+    {
+      description: 'should manage author with special chars',
+      option: 'author',
+      input: 'name <name@example.com>',
+      expected: '"name <name@example.com>"'
+    },
+    {
+      description: 'should manage version',
+      option: 'version',
+      input: '1.0.0',
+      expected: '1.0.0'
     }
   ].forEach((test) => {
     it(test.description, () => {
-      const option = new SchematicOption('name', test.input);
-      expect(option.toCommandString()).toEqual(`--name=${ test.expected }`);
+      const option = new SchematicOption(test.option, test.input);
+      expect(option.toCommandString()).toEqual(`--${ test.option }=${ test.expected }`);
     });
   });
 });
