24969c5e29cf154c0858c2fc5309a20893754b51
thomrick <thomasr2309@gmail.com>
2018-06-29 00:02:56 +0200

    task(tests) : initialize e2e test project

---
+++ e2e/src/features/new-command.spec.ts
@@ -0,0 +1,8 @@
+describe('New Command Specifications', () => {
+  it('should generate a new typescript Nest project', () => {
+    // execute command
+  });
+  it('should generate a new Javascript Nest project', () => {
+    // execute command
+  });
+});
