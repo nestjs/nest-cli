c7064ada0ab1e506666c30fc9d28125c5554f347
Kamil Myśliwiec <mail@kamilmysliwiec.com>
2019-09-16 14:27:08 +0200

    tools() add gulp and clean script

---
+++ tools/gulp/config.ts
@@ -0,0 +1,2 @@
+// All paths are related to the base dir
+export const sources = ['lib', 'actions', 'commands', 'bin'];
---
+++ tools/gulp/tasks/clean.ts
@@ -0,0 +1,31 @@
+import * as deleteEmpty from 'delete-empty';
+import { series, src, task } from 'gulp';
+import * as clean from 'gulp-clean';
+import { sources } from '../config';
+
+/**
+ * Cleans the build output assets from the packages folders
+ */
+function cleanOutput() {
+  const files = sources.map(source => [
+    `${source}/**/*.js`,
+    `${source}/**/*.d.ts`,
+    `${source}/**/*.js.map`,
+    `${source}/**/*.d.ts.map`,
+  ]);
+  return src(files.reduce((a, b) => a.concat(b), []), {
+    read: false,
+  }).pipe(clean());
+}
+
+/**
+ * Cleans empty dirs
+ */
+function cleanDirs(done: () => void) {
+  sources.forEach(source => deleteEmpty.sync(`${source}/`));
+  done();
+}
+
+task('clean:output', cleanOutput);
+task('clean:dirs', cleanDirs);
+task('clean:bundle', series('clean:output', 'clean:dirs'));
