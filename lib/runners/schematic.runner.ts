66e546741835703ade0fae57abd24e1a5cda6764
Andrei Alecu <andrei.alecu@vecttor.co>
2020-09-30 17:58:53 +0300

    refactor(cli): Schematic runner should use node require mechanism

--- lib/runners/schematic.runner.ts
+++ lib/runners/schematic.runner.ts
@@ -1,10 +1,8 @@
-import { existsSync } from 'fs';
-import { join, resolve } from 'path';
 import { AbstractRunner } from './abstract.runner';
 
 export class SchematicRunner extends AbstractRunner {
   constructor() {
-    super(`"${SchematicRunner.findClosestSchematicsBinary()}"`);
+    super(`node`, [`"${SchematicRunner.findClosestSchematicsBinary()}"`]);
   }
 
   public static getModulePaths() {
@@ -12,14 +10,10 @@ export class SchematicRunner extends AbstractRunner {
   }
 
   public static findClosestSchematicsBinary(): string {
-    const subPath = join('.bin', 'schematics');
-    for (const path of this.getModulePaths()) {
-      const binaryPath = resolve(path, subPath);
-      if (existsSync(binaryPath)) {
-        return binaryPath;
-      }
+    try {
+      return require.resolve('@angular-devkit/schematics-cli/bin/schematics.js', { paths: this.getModulePaths() });
+    } catch {
+      throw new Error("'schematics' binary path could not be found!");
     }
-
-    throw new Error("'schematics' binary path could not be found!");
   }
 }
