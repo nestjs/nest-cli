9f5f107990fb04ae8fb70b7bb6db4d613ab497d3
Pong <samfunghp@gmail.com>
2020-11-28 15:28:46 +0800

    feat(plugin): support after declarations

--- lib/ui/errors.ts
+++ lib/ui/errors.ts
@@ -4,5 +4,5 @@ export const CLI_ERRORS = {
   MISSING_TYPESCRIPT: (path: string) =>
     `Could not find TypeScript configuration file "${path}". Please, ensure that you are running this command in the appropriate directory (inside Nest workspace).`,
   WRONG_PLUGIN: (name: string) =>
-    `The "${name}" plugin is not compatible with Nest CLI. Neither "after()" nor "before()" function have been provided.`,
+    `The "${name}" plugin is not compatible with Nest CLI. Neither "after()" nor "before()" nor "afterDeclarations()" function have been provided.`,
 };
