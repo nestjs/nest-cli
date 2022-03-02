80cee87653c5f856abd54eb64b936eb6e20876de
Zlatin Stanimirov <zlatin.stanimirov@adeccogroup.com>
2022-03-02 16:22:24 +0200

    refactor(options): extract dasherize in util

--- lib/schematics/schematic.option.ts
+++ lib/schematics/schematic.option.ts
@@ -1,3 +1,5 @@
+import { dasherize } from '../utils/formatting';
+
 export class SchematicOption {
   constructor(private name: string, private value: boolean | string) {}
 
@@ -11,15 +13,15 @@ export class SchematicOption {
         return `--${this.name}="${this.value}"`;
       }
     } else if (typeof this.value === 'boolean') {
-      const str = this.dasherize(this.name);
+      const str = dasherize(this.name);
       return this.value ? `--${str}` : `--no-${str}`;
     } else {
-      return `--${this.dasherize(this.name)}=${this.value}`;
+      return `--${dasherize(this.name)}=${this.value}`;
     }
   }
 
   private format() {
-    return this.dasherize(this.value as string)
+    return dasherize(this.value as string)
       .split('')
       .reduce((content, char) => {
         if (char === '(' || char === ')' || char === '[' || char === ']') {
@@ -27,14 +29,5 @@ export class SchematicOption {
         }
         return `${content}${char}`;
       }, '');
-  }
-
-  private dasherize(str: string) {
-    const STRING_DASHERIZE_REGEXP = /[\s]/g;
-    const STRING_DECAMELIZE_REGEXP = /([a-z\d])([A-Z])/g;
-    return str
-      .replace(STRING_DECAMELIZE_REGEXP, '$1-$2')
-      .toLowerCase()
-      .replace(STRING_DASHERIZE_REGEXP, '-');
   }
 }
---
+++ lib/utils/formatting.ts
@@ -0,0 +1,8 @@
+export function dasherize(str: string) {
+  const STRING_DASHERIZE_REGEXP = /[\s]/g;
+  const STRING_DECAMELIZE_REGEXP = /([a-z\d])([A-Z])/g;
+  return str
+    .replace(STRING_DECAMELIZE_REGEXP, '$1-$2')
+    .toLowerCase()
+    .replace(STRING_DASHERIZE_REGEXP, '-');
+}
