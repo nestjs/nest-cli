2f057c2cba483c22e1a801ed9f9f3123a4248ef1
Zlatin Stanimirov <4281340+stanimirovv@users.noreply.github.com>
2022-03-03 12:58:01 +0200

    chore: remove whitespace
    Co-authored-by: Micael Levi L. Cavalcante <mllc@icomp.ufam.edu.br>

--- lib/utils/formatting.ts
+++ lib/utils/formatting.ts
@@ -5,7 +5,6 @@
  * @description normalizes input to supported path and file name format.
  * Changes camelCase strings to kebab-case, replaces spaces with dash and keeps underscores.
  */
-
 export function normalizeToKebabOrSnakeCase(str: string) {
   const STRING_DASHERIZE_REGEXP = /\s/g;
   const STRING_DECAMELIZE_REGEXP = /([a-z\d])([A-Z])/g;
