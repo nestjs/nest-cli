3568fbfc739c83c349ba25a8cc72d76a44f34efe
Kamil Myśliwiec <mail@kamilmysliwiec.com>
2023-02-03 12:59:48 +0100

    deps: upgrade rimraf

--- lib/compiler/workspace-utils.ts
+++ lib/compiler/workspace-utils.ts
@@ -16,8 +16,6 @@ export class WorkspaceUtils {
     if (!isDeleteEnabled) {
       return;
     }
-    await new Promise<void>((resolve, reject) =>
-      rimraf(dirPath, (err) => (err ? reject(err) : resolve())),
-    );
+    await rimraf(dirPath);
   }
 }
