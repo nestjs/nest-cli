34096ef5cd72f20560c505d4c55564a1c0e276da
Micael Levi L. Cavalcante <mllc@icomp.ufam.edu.br>
2023-10-09 11:46:51 -0400

    refactor: remove 'is-compilable-extension' non-used helper

--- lib/compiler/helpers/is-compilable-extension.ts
+++
@@ -1,9 +0,0 @@
-import { extname } from 'path';
-
-export function isCompilableExtension(
-  filename: string,
-  allowedExtension: string[],
-): boolean {
-  const ext = extname(filename);
-  return allowedExtension.includes(ext);
-}
