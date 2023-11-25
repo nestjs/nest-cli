ab55b2db008a03a144e3dfb21432a6708bba1f48
chamsou123 <agounichams@gmail.com>
2023-11-25 19:23:41 +0100

    refactor: Implement osName, remove os-name package

---
+++ lib/utils/os-info.utils.ts
@@ -0,0 +1,20 @@
+export default function osName(platform: string, release: string): string {
+  switch (platform) {
+    case 'darwin':
+      return Number(release.split('.')[0]) > 15 ? 'macOS' : 'OS X';
+    case 'linux':
+      return 'Linux';
+    case 'win32':
+      return 'Windows';
+    case 'freebsd':
+      return 'FreeBSD';
+    case 'openbsd':
+      return 'OpenBSD';
+    case 'sunos':
+      return 'Solaris';
+    case 'android':
+      return 'Android';
+    default:
+        return platform;
+  }
+}
