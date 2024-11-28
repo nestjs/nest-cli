59f2f53212f29e2d02efa02a49bfc0373f9b364c
bangyu.xiangby <bangyu.xiangby@alibaba-inc.com>
2024-11-28 16:12:58 +0800

    feat: 优化代码

--- lib/compiler/defaults/swc-defaults.ts
+++ lib/compiler/defaults/swc-defaults.ts
@@ -5,49 +5,53 @@ export const swcDefaultsFactory = (
   tsOptions?: ts.CompilerOptions,
   configuration?: Configuration,
 ) => {
-  const builderOptions =
-    typeof configuration?.compilerOptions?.builder !== 'string'
-      ? configuration?.compilerOptions?.builder?.options
+  // 获取构建器选项
+  const builderOptions = configuration?.compilerOptions?.builder && 
+    typeof configuration.compilerOptions.builder !== 'string' 
+      ? configuration.compilerOptions.builder.options 
       : {};
 
+  // SWC编译器选项
+  const swcOptions = {
+    sourceMaps: tsOptions?.sourceMap || (tsOptions?.inlineSourceMap && 'inline'),
+    module: { type: 'commonjs' },
+    jsc: {
+      target: 'es2021',
+      parser: {
+        syntax: 'typescript',
+        decorators: true,
+        dynamicImport: true,
+      },
+      transform: {
+        legacyDecorator: true,
+        decoratorMetadata: true,
+        useDefineForClassFields: false,
+      },
+      keepClassNames: true,
+      baseUrl: tsOptions?.baseUrl,
+      paths: tsOptions?.paths,
+    },
+    minify: false,
+    swcrc: true,
+  };
+
+  // CLI选项
+  const cliOptions = {
+    outDir: tsOptions?.outDir ? convertPath(tsOptions.outDir) : 'dist',
+    filenames: [configuration?.sourceRoot ?? 'src'],
+    sync: false,
+    extensions: ['.js', '.ts'],
+    copyFiles: false,
+    includeDotfiles: false,
+    quiet: false,
+    watch: false,
+    stripLeadingPaths: true,
+    ...builderOptions,
+  };
+
   return {
-    swcOptions: {
-      sourceMaps:
-        tsOptions?.sourceMap || (tsOptions?.inlineSourceMap && 'inline'),
-      module: {
-        type: 'commonjs',
-      },
-      jsc: {
-        target: 'es2021',
-        parser: {
-          syntax: 'typescript',
-          decorators: true,
-          dynamicImport: true,
-        },
-        transform: {
-          legacyDecorator: true,
-          decoratorMetadata: true,
-          useDefineForClassFields: false,
-        },
-        keepClassNames: true,
-        baseUrl: tsOptions?.baseUrl,
-        paths: tsOptions?.paths,
-      },
-      minify: false,
-      swcrc: true,
-    },
-    cliOptions: {
-      outDir: tsOptions?.outDir ? convertPath(tsOptions.outDir) : 'dist',
-      filenames: [configuration?.sourceRoot ?? 'src'],
-      sync: false,
-      extensions: ['.js', '.ts'],
-      copyFiles: false,
-      includeDotfiles: false,
-      quiet: false,
-      watch: false,
-      stripLeadingPaths: true,
-      ...builderOptions,
-    },
+    swcOptions,
+    cliOptions,
   };
 };
 
