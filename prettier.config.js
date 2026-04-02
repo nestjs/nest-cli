02701ae3e7b814815e0bdbf02db29427f0eba99f
Luciano Alves <luas10c@gmail.com>
2026-04-02 01:00:50 -0300

    chore: standardizing prettier configurations

---
+++ prettier.config.js
@@ -0,0 +1,18 @@
+/** @type{import('prettier').Config} */
+export default {
+  singleQuote: true,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  semi: false,
+  printWidth: 80,
+  quoteProps: 'preserve',
+  proseWrap: 'preserve',
+  arrowParens: 'always',
+  bracketSameLine: true,
+  endOfLine: 'lf',
+  singleAttributePerLine: false,
+  jsxSingleQuote: false,
+  useTabs: false,
+  tabWidth: 2,
+  plugins: [],
+};
